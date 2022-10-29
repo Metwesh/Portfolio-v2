2ef661db68969ebc9a18587e776a56452fe54357
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-10-29 23:08:55 +0200

    Fixed the Service worker

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,5 @@
 self.addEventListener("install", () => {});
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      // Enable navigation preload if it's supported.
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
-  );
-  
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
+self.addEventListener("activate", () => self.clients.claim());
 
-self.addEventListener("fetch", (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          // First, try to use the navigation preload response if it's supported.
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch {
-          /* */
-        }
-      })()
-    );
-  }
-
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
-});
+self.addEventListener("fetch", () => {});
