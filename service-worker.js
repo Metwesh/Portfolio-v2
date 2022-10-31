10e6aba04c7cc20ad1b2d67ffe37dccfa469a800
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-10-31 16:11:49 +0200

    Added proper CSS fallback for webp images

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
 self.addEventListener("install", () => {});
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    (async () => {
-      // Enable navigation preload if it's supported.
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
-  );
-  self.clients.claim();
-});
+self.addEventListener("activate", () => {});
 
 self.addEventListener("fetch", () => {});
