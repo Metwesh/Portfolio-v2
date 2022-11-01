a75fb009cd954d1640fb173ebd0d06d0d350e323
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-11-01 23:39:06 +0200

    Fixed the sliding triangle and added two more for you to find. Extinguished some bugs of course.

--- app/js/scroll-animation.js
+++ app/js/scroll-animation.js
@@ -1,40 +1,35 @@
 let lastKnownScrollPosition = 0;
 let ticking = false;
 const coolShape = document.querySelector("#coolShape");
+const tinyShape1 = document.querySelector("#tinyShape1");
+const tinyShape2 = document.querySelector("#tinyShape2");
 
 function scrollListener() {
   lastKnownScrollPosition = window.scrollY;
   if (!ticking) {
     window.requestAnimationFrame(function () {
-      scrollMove(lastKnownScrollPosition);
+      scrollMoveBigShape(lastKnownScrollPosition);
+      scrollMoveSmallShapes(lastKnownScrollPosition);
       ticking = false;
     });
     ticking = true;
   }
 }
 
-function scrollMove(scrollPos) {
+function scrollMoveBigShape(scrollPos) {
   if (screen.width > 1073) {
     coolShape.style.transform = `translateX(${-scrollPos * 0.9 - 100}px)`;
   } else if (screen.width > 600 && screen.width <= 1073) {
-    coolShape.style.transform = `translateX(${-scrollPos * 1.5 - 100}px)`;
+    coolShape.style.transform = `translateX(${-scrollPos * 1.1 - 100}px)`;
   } else if (screen.width >= 390 && screen.width <= 600) {
-    coolShape.style.transform = `translateX(${-scrollPos * 1.8 - 100}px)`;
+    coolShape.style.transform = `translateX(${-scrollPos * 1.3 - 100}px)`;
   } else {
-    coolShape.style.transform = `translateX(${-scrollPos * 2 - 100}px)`;
+    coolShape.style.transform = `translateX(${-scrollPos * 1.5 - 100}px)`;
   }
 }
 
+function scrollMoveSmallShapes(scrollPos) {
+  tinyShape1.style.transform = `translateX(${scrollPos * -0.5 - 500}px)`;
+  tinyShape2.style.transform = `translateX(${-scrollPos * -0.5 - 500}px)`;
+}
 document.addEventListener("scroll", scrollListener);
-
-const svgObserver = new IntersectionObserver((entries, _sectionObserver) => {
-  for (const entry of entries) {
-    if (!entry.isIntersecting) {
-      document.removeEventListener("scroll", scrollListener);
-    } else {
-      document.addEventListener("scroll", scrollListener);
-    }
-  }
-});
-
-svgObserver.observe(coolShape);
